01dd45081fde0c370939b963384d19126e715b70
LuckyShawn <LuckyShawn@foxmail.com>
2019-03-01 09:50:53 +0800

    Initial commit

---
+++ src/com/shawn/datastracture/SingleLinkedList.java
@@ -0,0 +1,10 @@
+package com.shawn.datastracture;
+
+/**
+ * @Description TODO
+ * @Author shawn
+ * @create 2019/3/1 0001
+ */
+public class SingleLinkedList implements MyList{
+
+}
---
+++ src/com/shawn/datastracture/SingleLinkedListTest.java
@@ -0,0 +1,14 @@
+package com.shawn.datastracture;
+
+import java.util.List;
+
+/**
+ * @Description TODO
+ * @Author shawn
+ * @create 2019/3/1 0001
+ */
+public class SingleLinkedListTest {
+    public static void main(String[] args){
+
+    }
+}
